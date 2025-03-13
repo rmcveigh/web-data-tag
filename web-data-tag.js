8c1e016bed3c73f79a72ff3975fbd9b1b1fa2139
rmcveigh <rmcveigh@morrisanimalfoundation.org>
2025-03-13 11:20:51 -0600

    Working on consent

--- web-data-tag.js
+++ web-data-tag.js
@@ -37,147 +37,49 @@ function dataTagParseResponse(str) {
 function dataTagSendData(data, gtmServerDomain, requestPath, dataLayerEventName, dataLayerVariableName, ucLocalStorageKey) {
     dataLayerEventName = dataLayerEventName || false;
     dataLayerVariableName = dataLayerVariableName || false;
-    const storedData = localStorage.getItem(ucLocalStorageKey);
-    if (storedData) {
-      // Parse data if it was stored as JSON
-      try {
-        const parsedData = JSON.parse(storedData);
-        // Use the retrieved data
-        console.log(parsedData);
-      } catch (error) {
-        // Handle the case where data is not valid JSON
-        console.error('Error parsing JSON:', error);
-        //If data was not JSON, use it as a string
-        console.log(storedData);
-      }
-    }
-    return;
-
-    var replaceVariable = function(a, b) {
-            return a.replace(/\$\{([^\}]+)\}/g, function(c, d) {
-                return b[d] || c
-            })
-        },
-        xhr = null,
-        sendPixel = function(url) {
-            var img = new Image(1, 1);
-            if (url.startsWith(((gtmServerDomain.charAt(gtmServerDomain.length - 1) === '/') ? gtmServerDomain.slice(0, -1) : gtmServerDomain) + '/_set_cookie')) {
-                setCookieRunningCount++;
-                img.onload = img.onerror = function() {
-                    img.onload = img.onerror = null;
-                    setCookieRunningCount--;
-                    if ((!xhr || xhr.readyState === 4) // server container must be finished to be sure no more cookies will be received
-                      && dataLayerEventName && dataLayerVariableName // data tag configured to push event
-                      && (setCookieRunningCount === 0) // all cookies already set
-                    ) {
-                        pushToDataLayer();
+    // Do not send anything if they have not consented to a service.
+    if (ucLocalStorageKey === 'ucData') {
+        const storedData = localStorage.getItem(ucLocalStorageKey);
+        let consentServices = {};
+        let okToSend = false;
+        if (storedData) {
+          // Parse data if it was stored as JSON
+          try {
+            const parsedData = JSON.parse(storedData);
+            // Use the retrieved data
+            consentServices = parsedData.consent.services || {};
+            okToSend = Object.keys(consentServices).length > 0;
+          } catch (error) {
+            // Handle the case where data is not valid JSON
+            console.error('Error parsing JSON:', error);
+          }
+        }
+        if (Object.keys(consentServices).length > 0) {
+            for (const service in consentServices) {
+                let consent = consentServices[service].consent || false;
+                let name = consentServices[service].name || '';
+                if ((name === 'Google Analytics' || name === '') && !consent) {
+                    okToSend = false;
+                    break;
+                }
+            }
+        }
+    } else if (ucLocalStorageKey === 'uc_settings' && dataLayer && dataLayer.length > 0) {
+        dataLayer.forEach(function (layer) {
+            if (typeof layer === 'object') {
+                Object.keys(layer).forEach(function (key) {
+                    if (key === 'Google Analytics') {
+                        okToSend = layer[key];
                     }
-                };
+                });
             }
-            img.src = url
-        },
-        sendBeacon = function(url) {
-            var sendBeaconResult;
-            try {
-                sendBeaconResult = navigator.sendBeacon && navigator.sendBeacon(url)
-            } catch (e) {}
-            sendBeaconResult || sendPixel(url)
-        },
-        fallbackIterator = function(a) {
-            var b = 0;
-            return function() {
-                return b < a.length ? {
-                    done: !1,
-                    value: a[b++]
-                } : {
-                    done: !0
-                }
-            }
-        },
-        pushToDataLayer = function() {
-            window[dataLayerVariableName] = window[dataLayerVariableName] || [];
-            eventDataLayerData.event = dataLayerEventName;
-            window[dataLayerVariableName].push(eventDataLayerData);
-        },
-        stringifiedData = JSON.stringify(data),
-        response = "",
-        loaded = 0,
-        replacements = {transport_url: gtmServerDomain},
-        eventDataLayerData = {},
-        setCookieRunningCount = 0,
-        processResponseDataEvent = function(event) {
-          if (event) {
-            var sendPixelArr = event.send_pixel || [],
-              i;
-            if (Array.isArray(sendPixelArr))
-              for (i = 0; i < sendPixelArr.length; i++) sendPixel(sendPixelArr[i]);
-
-            var sendBeaconArr = event.send_beacon || [];
-            if (Array.isArray(sendBeaconArr))
-              for (i = 0; i < sendBeaconArr.length; i++) sendBeacon(sendBeaconArr[i])
-
-            if (typeof event.response === 'object') {
-              var status = event.response.status_code || 0,
-                body = event.response.body || {},
-                parsedBody = dataTagParseResponse(body);
-
-              // merge parsedBody into eventDataLayerData instead of assignment
-              // (just in case multiple responses will be supported by gtm in the future)
-              for (var key in parsedBody) {
-                if (parsedBody.hasOwnProperty(key)) {
-                  eventDataLayerData[key] = parsedBody[key];
-                }
-              }
-              eventDataLayerData.status = status;
-            }
-          }
-        };
-      xhr = new XMLHttpRequest();
-      xhr.open('POST', gtmServerDomain + requestPath);
-      xhr.setRequestHeader('Content-type', 'text/plain');
-      xhr.withCredentials = true;
-
-      xhr.onprogress = function(progress) {
-        if ((xhr.status === 200) && xhr.responseText.startsWith("event: message\ndata: ")) {
-          response += xhr.responseText.substring(loaded);
-          loaded = progress.loaded;
-
-          for (var replacedResponse = replaceVariable(response, replacements), nextSeparationPos = replacedResponse.indexOf("\n\n"); - 1 !== nextSeparationPos;) {
-            var parsedData;
-            a: {
-              var iterableLines;
-              var lines = replacedResponse.substring(0, nextSeparationPos).split("\n"),
-                linesIterator = "undefined" != typeof Symbol && Symbol.iterator && lines[Symbol.iterator];
-              if (linesIterator) iterableLines = linesIterator.call(lines);
-              else if ("number" == typeof lines.length) iterableLines = {
-                next: fallbackIterator(lines)
-              };
-              else throw Error(String(lines) + " is not an iterable or ArrayLike");
-              var eventNameLine = iterableLines.next().value,
-                eventDataLine = iterableLines.next().value;
-              if (eventNameLine.startsWith("event: message") && eventDataLine.startsWith("data: ")) try {
-                parsedData = JSON.parse(eventDataLine.substring(eventDataLine.indexOf(":") + 1));
-                break a
-              } catch (e) {}
-              parsedData =
-                void 0
-            }
-            processResponseDataEvent(parsedData);
-            replacedResponse = replacedResponse.substring(nextSeparationPos + 2);
-            nextSeparationPos = replacedResponse.indexOf("\n\n")
-          }
-        }
-      };
-      xhr.onload = function () {
-        if (xhr.status.toString()[0] !== '2') {
-          console.error(xhr.status + '> ' + xhr.statusText);
-        }
-
-        if (dataLayerEventName && dataLayerVariableName) { // data tag configured to push event
-          pushToDataLayer();
-        }
-      };
-      // xhr.send(stringifiedData);
+        })
+    }
+    if (!okToSend) {
+      console.warn('User has not consented to Google Analytics');
+    } else {
+        console.log('User has consented to Google Analytics');
+    }
 }
 
 /**
